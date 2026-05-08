b7200aa095ea1b705fadf174d2fc1b9510e2fee5
erab2r <shkera213@gmail.com>
2026-05-08 20:00:37 +0600

    1

--- src/Polymorphism.ts
+++ src/Polymorphism.ts
@@ -23,3 +23,51 @@ const person2 = new Student()
 const person3 = new NextLevelDeveloper()
 
 getSleepingHours(person3);
+
+
+class Shape {
+  getArea(): number {
+    return 0;
+  }
+}
+
+class Circle extends Shape {
+  // area= pi*r*r
+  radius: number;
+
+  constructor(radius: number) {
+    super();
+    this.radius = radius;
+  }
+
+  getArea(): number {
+    return Math.PI * this.radius * this.radius;
+  }
+}
+
+class Rectangle extends Shape {
+  // area= height*width
+
+  height: number;
+  width: number;
+
+  constructor(height: number, width: number) {
+    super();
+    this.height = height;
+    this.width = width;
+  }
+
+  getArea(): number {
+    return this.height * this.width;
+  }
+}
+
+const getArea = (param: Shape) => {
+  console.log(param.getArea());
+};
+
+const shape1 = new Shape();
+const shape2 = new Circle(10);
+const shape3 = new Rectangle(10, 20);
+
+getArea(shape3);
